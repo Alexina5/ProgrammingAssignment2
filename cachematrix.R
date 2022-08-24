e2052223f23a6f0ba07459fc8ef7b2ff26bb7e3c
Alexina5 <106883867+Alexina5@users.noreply.github.com>
2022-08-24 21:38:41 +0300

    My Coursera assignment
    This task was performed on 24.08.2022 as a part of Coursera's Programming Assignment 2: Lexical Scoping.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##      According the the assignment the following functions will be used:
+##      makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
+##      cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+##      If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates matrix which can cache its inverse:
+makeCacheMatrix <- function(mtrx = matrix()) {
+        inv <- NULL
+        set <- function(matrix) {
+                mtrx <<- matrix
+                inv <<- NULL
+        }
+        get <- function() {
+                mtrx
+        }
+        setInverse <- function(inverse) {
+                inv <<- inverse
+        }
+        getInverse <- function() {
+                inv
+        }
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Next function computes inverse of the matrix returned by the makeCacheMatrix function. 
+## cacheSolve function is also able to get an inverse from cache in some conditions.
+cacheSolve <- function(mtrx, ...) {
+        ## Return a matrix that is the inverse of 'mtrx'
+        inv <- x$getInverse()
+        if(!is.null(inv)) {
+                message("Attention: cached data will be obtained")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setInverse(inv)
+        inv
 }
+
+## The task performed as a Coursera programming assignment. Thank you for your attention.
